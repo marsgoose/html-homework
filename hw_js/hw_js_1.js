1e7560d5fc82207ecf26e30b1b848df3d6e0bad3
maksi <maksimys020304@gmail.com>
2023-11-21 14:53:00 +0300

    add js

---
+++ hw_js/hw_js_1.js
@@ -0,0 +1,38 @@
+let s1='hello'
+console.log(s1)
+let s2='owu'
+console.log(s2)
+let s3='com'
+console.log(s3)
+let s4='ua'
+console.log(s4)
+let n1=1
+console.log(n1)
+let n2=10
+console.log(n2)
+let n3=-999
+console.log(n3)
+let n4=123
+console.log(n4)
+let n5=3.14
+console.log(n5)
+let n6=2.7
+console.log(n6)
+let n7=16
+console.log(n7)
+let b1=true
+console.log(b1)
+let b2=false
+console.log(b2)
+console.log('----------')
+let firstName='Maksym'
+let middleName='Vitaliyovych'
+let lastName='Borysenko'
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+console.log('----------')
+let a=100
+console.log(typeof a)
+let b='100'
+console.log(typeof b)
+let c=true
+console.log(typeof c)
